21afdac8ce49063c9a5de6f7b259e26db13ee94a
unknown <--local>
2023-08-15 16:58:02 -0500

    Cleaned script and added boundary feature

--- image_watermark.py
+++ image_watermark.py
@@ -1,24 +1,29 @@
 from PIL import Image, ImageDraw
 
-def generate_layer(repeat = True, transparency = 75, offset = (0, 0)):
-    layer = Image.new('RGBA', (1000, 100), (255, 0, 255, 50)) #Add dimensions
+# Generates the watermark as a layer to be added over image.
+def generate_layer(repeat = True, transparency = 75, offset = (0, 0), boundary = True):
+    layer = Image.new('RGBA', (1000, 100), (255, 255, 255, 0)) #Add dimensions
+    maxx, maxy = layer.size
+
+    if boundary:
+        d_boundary = ImageDraw.Draw(layer)
+        d_boundary.line([(0,0), (maxx,0), (maxx, maxy), (0, maxy), (0,0)], fill=(0, 0, 0), width = 5)
 
     # Generate the Watermark
-    i = Image.new('RGBA', (100, 100), (255, 255, 0, 50))
-    d = ImageDraw.Draw(i)
-    d.text((10, 15), 'Artisan Market', fill = (255, 255, 255, 255))
-    d.text((10, 30), 'Two Ole Bs', fill = (0, 255, 0, 255))
+    #if mark is image
+    #    mark = image
+    # else # mark is text
+    mark = Image.new('RGBA', (100, 100))
+    d_mark = ImageDraw.Draw(mark)
+    d_mark.text((10, 15), 'Artisan Market', fill = (0, 0, 0))
 
     drawn = layer.copy()
-    drawn.alpha_composite(i)
-    drawn.alpha_composite(i, dest = (100, 0))
-    drawn.alpha_composite(i, dest = (200, 0))
-    #drawn = Image.alpha_composite(layer, i)
+    drawn.alpha_composite(mark)
+    # Repeat Watermark Across Image
+    #for x,y in ?
+    #    drawn.alpha_composite(i, dest = (x * size, y * size))
 
     # Debug
-    layer.show() # Start of image
-    i.show()
-    #drawn.show() #Single Text Instance
     drawn.show() #Full Watermark
 
     return layer
